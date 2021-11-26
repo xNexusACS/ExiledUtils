cf306017ac101b14b4462384fd269a8d5b16f31f
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-11-26 22:50:07 +0100

    Deleted old desc

--- ExiledUtils/Configs/InfiniteAmmoConfig.cs
+++ ExiledUtils/Configs/InfiniteAmmoConfig.cs
@@ -4,7 +4,7 @@ namespace ExiledUtils.Configs
 {
     public class InfiniteAmmoConfig
     {
-        [Description("Enable infinite ammo? (Have some bugs, activate at your own risk)")]
+        [Description("Enable infinite ammo?")]
         public bool InfiniteAmmoEnable { get; set; } = false;
     }
 }
