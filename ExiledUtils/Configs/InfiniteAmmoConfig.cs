6560bb03374d5386b4eb9e12c0c74b9a42c6c499
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-11-26 10:54:07 +0100

    Add files via upload

---
+++ ExiledUtils/Configs/InfiniteAmmoConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace ExiledUtils.Configs
+{
+    public class InfiniteAmmoConfig
+    {
+        [Description("Amount of ammo to add when shooting with a gun")]
+        public ushort AmmoValues { get; set; } = 3000;
+        [Description("Enable infinite ammo? (Have some bugs, activate at your own risk)")]
+        public bool InfiniteAmmoEnable { get; set; } = false;
+    }
+}
