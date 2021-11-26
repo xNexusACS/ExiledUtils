a7a6f2e5568c5e7261cbc31974bf031cc800beda
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-11-26 22:47:06 +0100

    New Feature

---
+++ ExiledUtils/Configs/AddingTargetConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ExiledUtils.Configs
+{
+    public class AddingTargetConfig
+    {
+        [Description("Hint that appear when the target see the 096 face")]
+        public string TargetHint { get; set; } = "<color=red> Now you are a target for 096! </color>";
+        [Description("Hint that appear to 096")]
+        public string Scp096Hint { get; set; } = "<color=red> A player looked at your face </color>";
+        [Description("Hint duration")]
+        public float HintsDuration { get; set; } = 5;
+    }
+}
