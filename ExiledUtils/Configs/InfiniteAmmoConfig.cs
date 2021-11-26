0d868f37540a049bfe99c41769612df90fa8ca48
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-11-26 22:47:22 +0100

    New Feature

---
+++ ExiledUtils/Configs/InfiniteAmmoConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace ExiledUtils.Configs
+{
+    public class InfiniteAmmoConfig
+    {
+        [Description("Enable infinite ammo? (Have some bugs, activate at your own risk)")]
+        public bool InfiniteAmmoEnable { get; set; } = false;
+    }
+}
