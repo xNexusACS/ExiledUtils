a825ea9769f0f40d2262139ab1a720fbead84beb
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-03-17 11:31:24 +0100

    Downgrade to 3.0.3

--- ExiledUtils_REMAKE/EventHandlers.cs
+++ ExiledUtils_REMAKE/EventHandlers.cs
@@ -89,45 +89,5 @@ namespace ExiledUtils_REMAKE
                 ev.Scp049.EnableEffect(EffectType.MovementBoost, 10);
             }
         }
-        // RainbowTags
-        private bool TryGetColors(string rank, out string[] availableColors)
-        {
-            availableColors = this.plugin.Config.RTSequences;
-            return !string.IsNullOrEmpty(rank) && this.plugin.Config.RoleRainbowTags.Contains(rank);
-        }
-        private bool EqualsTo(UserGroup @this, UserGroup other)
-        {
-            return @this.BadgeColor == other.BadgeColor && @this.BadgeText == other.BadgeText && @this.Permissions == other.Permissions && @this.Cover == other.Cover && @this.HiddenByDefault == other.HiddenByDefault && @this.Shared == other.Shared && @this.KickPower == other.KickPower && @this.RequiredKickPower == other.RequiredKickPower;
-        }
-        private string GetGroupKey(UserGroup group)
-        {
-            if (group == null)
-            {
-                return string.Empty;
-            }
-            return ServerStatic.PermissionsHandler._groups.FirstOrDefault((KeyValuePair<string, UserGroup> g) => this.EqualsTo(g.Value, group)).Key ?? string.Empty;
-        }
-        public void OnChangingGroup(ChangingGroupEventArgs ev)
-        {
-            if (!ev.IsAllowed)
-            {
-                return;
-            }
-            string[] colors;
-            bool flag = this.TryGetColors(this.GetGroupKey(ev.NewGroup), out colors);
-            if (ev.NewGroup != null && ev.Player.Group == null && flag)
-            {
-                RainbowTagController rtController = ev.Player.GameObject.AddComponent<RainbowTagController>();
-                rtController.Colors = colors;
-                rtController.Interval = this.plugin.Config.ColorInterval;
-                return;
-            }
-            if (flag)
-            {
-                ev.Player.GameObject.GetComponent<RainbowTagController>().Colors = colors;
-                return;
-            }
-            UnityEngine.Object.Destroy(ev.Player.GameObject.GetComponent<RainbowTagController>());
-        }
     }
 }
