da61be19b82bd3721fd635158044c1f7dec8eccf
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-20 03:28:00 +0100

    Version Bump

--- ExiledUtils_REMAKE/MainClass.cs
+++ ExiledUtils_REMAKE/MainClass.cs
@@ -15,9 +15,9 @@ namespace ExiledUtils_REMAKE
         public override string Author { get; } = "xNexus-ACS";
         public override string Name { get; } = "ExiledUtils-Remake";
         public override string Prefix { get; } = "exiled_utils_remake";
-        public override Version Version { get; } = new Version(4, 2, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
-        public const VersionType type = VersionType.Remake;
+        public override Version Version { get; } = new Version(5, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(6, 0, 0);
+        public const VersionType type = VersionType.RemakeBeta;
         
         private static Harmony harmony;
 
